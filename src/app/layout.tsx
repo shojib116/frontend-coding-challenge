51fbaec79d3a418fbbbbcad21fa57a125f7eb726
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-11-11 15:08:51 +0600

    Fix: Implemented challenges

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
+import { headers } from "next/headers";
 import { UserAgentProvider } from "../components/providers/userAgentProvider";
 import "./globals.css";
 import { Layout } from "@/components/layout";
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const userAgent = headers().get("user-agent") || "Unknown";
+
   return (
     <html lang="en">
       <body>
-        <UserAgentProvider>
+        <UserAgentProvider userAgent={userAgent}>
           <Layout>{children}</Layout>
         </UserAgentProvider>
       </body>
